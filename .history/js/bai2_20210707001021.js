7a07c9bd3eb7c66261c425d39a6e139d3304e7ec
dovanthao <dovanthao2404@gmail.com>
2021-07-07 14:03:13 +0700

    done lan 2

---
+++ .history/js/bai2_20210707001021.js
@@ -0,0 +1,122 @@
+const LEVEL_1 = 50;
+const LEVEL_2 = 50;
+const LEVEL_3 = 100;
+const LEVEL_4 = 150;
+
+const MONEY_LEVEL_1 = 500;
+const MONEY_LEVEL_2 = 650;
+const MONEY_LEVEL_3 = 850;
+const MONEY_LEVEL_4 = 1100;
+const MONEY_LEVEL_5 = 1300;
+
+
+
+function getEle(idEle) {
+  return document.getElementById(idEle);
+}
+
+var notifications = [
+  "Vui lòng nhập tên người dùng!",
+  "Vui lòng nhập số kw sử dụng!",
+  "Số kw người dùng phải là số!",
+  "Số kw không âm!",
+]
+
+// Kiểm tra người dùng có nhập không
+function checkEntry(idCheck, idNotification, indexNotification) {
+  var contentCheck = getEle(idCheck).value;
+  var notification = getEle(idNotification);
+  if (contentCheck === '') {
+    notification.innerHTML = notifications[indexNotification];
+    return false;
+  } else {
+    notification.innerHTML = '';
+    return true;
+  }
+}
+
+// Kiểm tra có phải là số không
+function checkIsNumber(idCheck, idNotification, indexNotification) {
+  var number = +getEle(idCheck).value;
+  var notification = getEle(idNotification);
+  if (isNaN(number)) {
+    notification.innerHTML = notifications[indexNotification];
+    return false;
+  }
+  notification.innerHTML = '';
+  return true;
+}
+
+// Kiểm tra giá giá trị của điểm
+function checkValueKw(idCheck, idNotification, indexNotification) {
+  var kw = +getEle(idCheck).value;
+  var notification = getEle(idNotification);
+  if (kw < 0) {
+    notification.innerHTML = notifications[indexNotification];
+    return false;
+  }
+  notification.innerHTML = '';
+  return true;
+}
+
+
+function checkValid() {
+  var result = checkEntry("username", "notificationUsername", 0);
+
+  var result1 = checkEntry("kwUse", "notificationKWUse", 1) &&
+    checkIsNumber("kwUse", "notificationKWUse", 2) &&
+    checkValueKw("kwUse", "notificationKWUse", 3);
+
+  return (result && result1) ? true : false;
+}
+
+function getTotalLevel1(kw) {
+  return kw * MONEY_LEVEL_1;
+}
+
+function getTotalLevel2(kw) {
+  var totalLevel1 = getTotalLevel1(50);
+  return (kw - 50) * MONEY_LEVEL_2 + totalLevel1;
+}
+
+function getTotalLevel3(kw) {
+  var totalVevel2 = getTotalLevel2(100);
+  return (kw - 100) * MONEY_LEVEL_3 + totalVevel2;
+}
+
+function getTotalLevel4(kw) {
+  var totalLevel3 = getTotalLevel3(200);
+  return (kw - 200) * MONEY_LEVEL_4 + totalLevel3;
+}
+
+function getTotalLevel5(kw) {
+  var totalLevel4 = getTotalLevel4(350);
+  return (kw - 350) * MONEY_LEVEL_5 + totalLevel4;
+
+}
+
+function getTotalMoney(kw) {
+  if (kw <= 50) {
+    return getTotalLevel1(kw);
+  }
+  if (kw <= 100) {
+    return getTotalLevel2(kw);
+  }
+  if (kw <= 200) {
+    return getTotalLevel3(kw);
+  }
+  if (kw <= 350) {
+    return getTotalLevel4(kw);
+  }
+  return getTotalLevel5(kw);
+}
+
+getEle("btnTotalAmount").addEventListener("click", function () {
+  var result = checkValid();
+  getEle('resultAmount').innerHTML = '';
+  if (result) {
+    var kw = +getEle("kwUse").value;
+    var totalMoney = getTotalMoney(kw);
+    getEle('resultAmount').innerHTML = "Tổng tiền là: " + totalMoney.toFixed(2) + " VND";
+  }
+})
---
+++ .history/js/bai4_20210707013025.js
@@ -0,0 +1,206 @@
+
+const INVOICE_HANDLING_FEE_PERSONAL = 4.5;
+const BASIC_SERVICE_FEE_PERSONAL = 20.5;
+const PREMIUM_SERVICE_FEE_PERSONAL = 7.5;
+
+
+const INVOICE_HANDLING_FEE_ENTERPRISE = 15;
+const BASIC_SERVICE_FEE_ENTERPRISE_FIRST = 7.5;
+const BASIC_SERVICE_FEE_ENTERPRISE_SECOND = 5;
+const PREMIUM_SERVICE_FEE_ENTERPRISE = 50;
+
+
+// Lấy element từ id
+function getEle(id) {
+  return document.getElementById(id);
+}
+
+// Các thông báo lỗi
+var notifications = [
+  "Vui lòng nhập vào mã khách hàng!",
+  "Vui lòng chọn loại khách hàng!",
+  "Vui lòng nhập số kết nối!",
+  "Vui lòng nhập số kênh cao cấp!",
+  "Số kết nối phải là số và phải là số nguyên!",
+  "Số kênh cao cấp phải là số và phải là số nguyên!",
+  "Số kết nối không phải là số âm",
+  "Số kênh cao cấp không phải là số âm!",
+  "Số kết nối là số nguyên!",
+  "Số kênh cao cấp là số nguyên!",
+  "Mã khách hàng không chứa các ký tự đặc biệt!"
+]
+
+// Kiểm tra người dùng có nhập không
+function checkEntry(idCheck, idNotification, indexNotification) {
+  var contentCheck = getEle(idCheck).value;
+  var notification = getEle(idNotification);
+  if (contentCheck === '') {
+    notification.innerHTML = notifications[indexNotification];
+    return false;
+  }
+  notification.innerHTML = "";
+  return true;
+}
+
+
+// Kiểm tra người dùng nhập vào có phải là số không
+function checkIsNumber(idCheck, idNotification, indexNotification) {
+  var number = +getEle(idCheck).value;
+  var notification = getEle(idNotification);
+  if (isNaN(number)) {
+    notification.innerHTML = notifications[indexNotification];
+    return false;
+  }
+  notification.innerHTML = notifications[indexNotification];
+  return true;
+}
+
+// kiểm tra người dùng nhập vào số có đúng điều kiện không.
+function checkValueNumber(idCheck, idNotification, indexNotification) {
+  var number = +getEle(idCheck).value;
+  var notification = getEle(idNotification);
+  if (number < 0) {
+    notification.innerHTML = notifications[indexNotification];
+    return false;
+  }
+  notification.innerHTML = '';
+  return true;
+}
+
+// Kiểm tra có phải là số nguyên không
+function checkInteger(idCheck, idNotification, indexNotification) {
+  var number = +getEle(idCheck).value;
+  var notification = getEle(idNotification);
+  if (number % 1 === 0) {
+    notification.innerHTML = '';
+    return true;
+  }
+  notification.innerHTML = notifications[indexNotification];
+  return false;
+}
+
+
+// Xử lý disabled số kết nối
+getEle("personal").addEventListener("click", function () {
+  var personal = getEle("personal");
+  connectionNumber.setAttribute("disabled", '');
+  getEle("connectionNumber").value = "";
+  getEle("notificationConnection").style.display = "none";
+})
+
+// enabled số kết nối
+getEle("enterprise").addEventListener("click", function () {
+  var enterprise = getEle("enterprise");
+  connectionNumber.removeAttribute("disabled");
+  getEle("notificationConnection").style.display = "block";
+  getEle("notificationConnection").innerHTML = "";
+})
+
+// check select loại khách hàng
+function checkCustomerType(idNotification, indexNotification) {
+
+  var customerType = getCustomerType();
+  var notification = getEle(idNotification);
+  if (customerType === 0 || customerType === 1) {
+    notification.innerHTML = '';
+    return customerType;
+  }
+  getEle("notificationType").innerHTML = notifications[indexNotification];
+  return false;
+}
+
+// Lấy loại khách hàng
+function getCustomerType() {
+  var personal = getEle("personal").checked;
+  return (personal) ? 0 : 1;
+}
+
+// Kiểm tra mã khách hàng
+function checkCustomerCode(idCheck, idNotification, indexNotification) {
+  var codes = /^([\w])+$/;
+  var code = +getEle(idCheck).value;
+  var notification = getEle(idNotification);
+  if (!(code.match(codes))) {
+    notification.innerHTML = '';
+    return true;
+  }
+  notification.innerHTML = notifications[indexNotification];
+  return false;
+}
+
+// Kiểm tra tất cả giá trị có hợp lệ không
+function checkValid() {
+  var result1 = checkEntry("customerCode", "notificationCode", 0) &&
+    checkCustomerCode("customerCode", "notificationCode", 10);
+
+  var result2 = checkCustomerType('notificationType', 1);
+
+  var result4 = checkEntry("premiumChannel", "notificationPremium", 3) &&
+    checkIsNumber("premiumChannel", "notificationPremium", 5) &&
+    checkValueNumber("premiumChannel", "notificationPremium", 7) &&
+    checkInteger("premiumChannel", "notificationPremium", 9);
+
+  if (result2) {
+
+    var result3 = checkEntry("connectionNumber", "notificationConnection", 2) &&
+      checkIsNumber("connectionNumber", "notificationConnection", 4) &&
+      checkValueNumber("connectionNumber", "notificationConnection", 6) &&
+      checkInteger("connectionNumber", "notificationConnection", 8);
+
+    return (result1 && result2 && result3 && result4) ? true : false;
+  }
+
+  return (result1 && !result2 && result4) ? true : false;
+}
+
+function getNumberChannelPremium() {
+  return +getEle("premiumChannel").value;
+}
+
+// tính tiền cáp cá nhân
+function calculateCableFeePersonal() {
+  var premiumChannel = getNumberChannelPremium();
+  var total = INVOICE_HANDLING_FEE_PERSONAL + BASIC_SERVICE_FEE_PERSONAL + (PREMIUM_SERVICE_FEE_PERSONAL * premiumChannel);
+  return total;
+}
+
+// Tính tiền doanh nghiệp
+function calculateCableFeeEnterprise() {
+  var premiumChannel = getNumberChannelPremium();
+  var connectionNumber = +getEle("connectionNumber").value;
+  var total;
+  var firstConnections = 10;
+
+  if (connectionNumber <= firstConnections) {
+    total = INVOICE_HANDLING_FEE_ENTERPRISE + BASIC_SERVICE_FEE_ENTERPRISE_FIRST * connectionNumber +
+      premiumChannel * PREMIUM_SERVICE_FEE_ENTERPRISE;
+    return total;
+  }
+
+  total = INVOICE_HANDLING_FEE_ENTERPRISE + BASIC_SERVICE_FEE_ENTERPRISE_FIRST * firstConnections +
+    (BASIC_SERVICE_FEE_ENTERPRISE_SECOND * (connectionNumber - firstConnections)) +
+    premiumChannel * PREMIUM_SERVICE_FEE_ENTERPRISE;
+  return total;
+}
+
+// Xử lý xuất kết quả
+getEle("btnTotalMoney").addEventListener("click", function () {
+  var check = checkValid();
+  var resultContent = getEle("resultContent");
+  resultContent.innerHTML = '';
+  if (check) {
+    var total;
+    var customerType = getCustomerType();
+
+    if (customerType === 0) {
+      total = calculateCableFeePersonal();
+      resultContent.innerHTML = "Tổng số tiền phải trả là: " + total + "$";
+    } else {
+      total = calculateCableFeeEnterprise();
+      resultContent.innerHTML = "Tổng số tiền phải trả là: " + total + "$";
+    }
+  }
+})
+
+
+
